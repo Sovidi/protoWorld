c0b9c31d24bd525f418b1fe02fe649c9bb60b9a6
Sovidi <lim132445@gmail.com>
2024-07-14 18:15:56 +0900

    hs nohome nobasename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <Context>
       <section className="main">
-          <BrowserRouter basename="protoWorld">
+          <HashRouter>
             <Header/>
             <Routes>
               <Route path="/" element={<Main/>}/>
@@ -21,7 +21,7 @@ function App() {
               <Route path="/selectgame" element={<SelectGame/>}/>
               <Route path="/questiongame" element={<QuestionGame/>}/>
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
       </section>
     </Context>
   );
